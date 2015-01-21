b5be63d9ac243478c658df55539074fc8e030521
João S. O. Bueno <gwidion@gmail.com>
2015-01-20 23:21:43 -0200

    Wrong way

--- game/__init__.py
+++ game/__init__.py
@@ -1,8 +1,11 @@
 from browser import document, html, timer
 
+import time 
+
 SIZE = WIDTH, HEIGHT = 640, 480
 SCREEN = None
 CTX = None
+SHIPSIZE = 30
 
 def init():
     global SCREEN, CTX
@@ -12,9 +15,15 @@ def init():
     document.body.append(SCREEN)
     CTX = SCREEN.getContext("2d");
 
-
-
+def main():
+    pos = [320, 240]
+    #while True:
+        CTX.fillStyle = "green";
+        CTX.fillRect(pos[0], pos[1], SHIPSIZE, SHIPSIZE)
+        time.sleep(100) # does not work, actually
+        pos[0] += 1
+        SCREEN.width = WIDTH
 
 init()
 # menu()
-#main()
+main()

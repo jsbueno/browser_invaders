cc9e7294111787ce9c2405273054c1635d98615b
João S. O. Bueno <gwidion@gmail.com>
2015-01-20 23:12:23 -0200

    on screen

--- game/__init__.py
+++ game/__init__.py
@@ -1,5 +1,20 @@
+from browser import document, html, timer
+
+SIZE = WIDTH, HEIGHT = 640, 480
+SCREEN = None
+CTX = None
+
 def init():
-    print("On air")
+    global SCREEN, CTX
+    document.body.append(html.H1("Space Invaders - a Python adventure"))
+    SCREEN = html.CANVAS(width=640, height=480)
+    SCREEN.style = {"background": "black"}
+    document.body.append(SCREEN)
+    CTX = SCREEN.getContext("2d");
+
+
 
 
 init()
+# menu()
+#main()
